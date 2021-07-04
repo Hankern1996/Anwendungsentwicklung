cdc75bf5bc979ee75069ee3ba39ae375953e2e9c
Paulina0710 <paulina.wittich@gmx.de>
2021-07-04 18:17:01 +0200

    Hoffnung

--- eMobility_Style/ui.R
+++ eMobility_Style/ui.R
@@ -24,12 +24,12 @@ shinyUI(navbarPage(title = "eLectrify",
                             includeHTML("home.html"),
                             tags$script(src = "plugins/scripts.js"),
                             tags$head(
-                                tags$link(rel = "stylesheet", 
-                                          type = "text/css", 
-                                          href = "plugins/font-awesome-4.7.0/css/font-awesome.min.css"),
-                                tags$link(rel = "icon", 
-                                          type = "image/png", 
-                                          href = "images/logo_icon_e.png")
+                              tags$link(rel = "stylesheet", 
+                                        type = "text/css", 
+                                        href = "plugins/font-awesome-4.7.0/css/font-awesome.min.css"),
+                              tags$link(rel = "icon", 
+                                        type = "image/png", 
+                                        href = "images/logo_icon_e.png")
                             )
                    ),
                    
@@ -39,11 +39,11 @@ shinyUI(navbarPage(title = "eLectrify",
                             includeHTML("scrollToTop.html"),
                             
                             sidebarLayout(
-                              sidebarPanel(h3("Inbetriebnahme von Ladepunkten pro Jahr"),
+                              sidebarPanel(h4("Inbetriebnahme von Ladepunkten pro Jahr"),
                                            selectizeInput("year", "Wähle ein Jahr", choices = NULL)),
                               
                               mainPanel(
-                                
+    
                                             leafletOutput("map")                                         
                               
                             )
@@ -55,13 +55,25 @@ shinyUI(navbarPage(title = "eLectrify",
                    # tab panel 3 - Analysen
                    tabPanel("Analysen",
                             sidebarLayout(
-                              sidebarPanel(h3("Inbetriebnahme von Ladepunkten pro Bundesland"),
+                              sidebarPanel(h4("Inbetriebnahme von Ladepunkten pro Bundesland"),
                                            selectizeInput("country", "Wähle Bundesland", choices = NULL)),
                               
                               mainPanel(
                                 tabsetPanel(type = "tabs",
                                             tabPanel("Barplot", plotlyOutput("barplot")),
                                             tabPanel("Data", tableOutput("datahead"))),
+                              )
+                            ),
+                            
+                            sidebarLayout(
+                              sidebarPanel(h4("Top 10 Städte"),
+                                           selectInput("checkYear", "Wähle Jahr", choices = NULL)
+                              ),
+                              
+                              mainPanel(
+                                tabsetPanel(type = "tabs",
+                                            tabPanel("Barplot", plotlyOutput("barplot1")),
+                                            tabPanel("Data", tableOutput("datahead1"))),
                               )
                             )
                             #propertyComparison()
@@ -73,10 +85,10 @@ shinyUI(navbarPage(title = "eLectrify",
                             includeHTML("about.html"),
                             shinyjs::useShinyjs(),
                             tags$head(
-                                tags$link(rel = "stylesheet", 
-                                          type = "text/css", 
-                                          href = "plugins/carousel.css"),
-                                tags$script(src = "plugins/holder.js")
+                              tags$link(rel = "stylesheet", 
+                                        type = "text/css", 
+                                        href = "plugins/carousel.css"),
+                              tags$script(src = "plugins/holder.js")
                             ),
                             tags$style(type="text/css",
                                        ".shiny-output-error { visibility: hidden; }",
