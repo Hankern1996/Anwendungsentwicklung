e026a0f8d446b1253a6f8cf29f6835ae70310e07
Paulina0710 <paulina.wittich@gmx.de>
2021-07-17 12:09:51 +0200

    nix

--- eMobility_Style/ui.R
+++ eMobility_Style/ui.R
@@ -63,6 +63,8 @@ shinyUI(navbarPage(title = "eLectrify",
                             )
                             
                             
+                            
+                            
                    )),
                    
                    # ----------------------------------
